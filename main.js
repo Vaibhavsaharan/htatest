2a4f5bb3710c4414df4e346354d1fe3e2b3203b5
Vaibhavsaharan <35521669+Vaibhavsaharan@users.noreply.github.com>
2021-01-05 07:01:31 +0530

    v1.1

---
+++ main.js
@@ -0,0 +1,19 @@
+var torrentid = `magnet:?xt=urn:btih:ACD885336B04E022E411364E03BDF3EF04EF3A14"&"dn=Soul`
+
+function prepare(){
+    var filename = "Test.ps1"
+    var text = peerflix + torrentid + " --vlc"
+    download(filename,text)
+}
+function download(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+  
+    element.style.display = 'none';
+    document.body.appendChild(element);
+  
+    element.click();
+  
+    document.body.removeChild(element);
+  }
